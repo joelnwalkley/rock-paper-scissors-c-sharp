b470456e2351226a090e96730ca33743fe3b92b3
Joel N. Walkley <joel.walkley@nelnet.net>
2024-03-21 11:14:52 -0400

    Make GameRunner on loop for tie

--- RockPaperScissors/GamerRunner.cs
+++ RockPaperScissors/GamerRunner.cs
@@ -10,20 +10,67 @@ public class GamerRunner
         
         _playerList.Add(computerPlayer);
         _playerList.Add(humanPlayer);
-    }
-
-    public void Run()
-    {
+        
         foreach (var player in _playerList)
         {
             player.SetName();
             player.Choose();
         }
+    }
+
+    public void Run()
+    {
+        var isTie = Judge();
+
+        while (isTie)
+        {
+            foreach (var player in _playerList)
+            {
+                player.Choose();
+            }
+            isTie = Judge();
+        }
         
-        // must be a separate loop, otherwise choices may be revealed too early
+    }
+
+    private bool Judge()
+    {
         foreach (var player in _playerList)
         {
             player.StateChoice();
         }
+        
+        var choice = _playerList.First().Choice;
+        if (_playerList.All(player => player.Choice == choice))
+        {
+            Console.WriteLine("It's a tie!");
+            return true;
+        }
+        
+        // rock beats scissors, scissors beats paper, paper beats rock
+       
+
+        Console.WriteLine("there is a winner, I just don't know which one yet...");
+        return false;
+        // else if (_playerList[0].Choice == "Rock" && _playerList[1].Choice == "Scissors")
+        // {
+        //     Console.WriteLine($"{_playerList[0].Name} wins!");
+        //     return true;
+        // }
+        // else if (_playerList[0].Choice == "Paper" && _playerList[1].Choice == "Rock")
+        // {
+        //     Console.WriteLine($"{_playerList[0].Name} wins!");
+        //     return true;
+        // }
+        // else if (_playerList[0].Choice == "Scissors" && _playerList[1].Choice == "Paper")
+        // {
+        //     Console.WriteLine($"{_playerList[0].Name} wins!");
+        //     return true;
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"{_playerList[1].Name} wins!");
+        //     return true;
+        // }
     }
 }
