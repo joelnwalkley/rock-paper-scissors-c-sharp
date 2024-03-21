d4b973c806b7b79783c3771605abaf61c5e7c69c
Joel N. Walkley <joel.walkley@nelnet.net>
2024-03-21 09:27:03 -0400

    use generic list example

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,7 +4,14 @@ using RockPaperScissors;
 
 Console.WriteLine("Hello, World!");
 
-var player = new Computer("Computer");
-player.Choose();
+var player1 = new Computer("Computer1");
+var player2 = new Computer("Computer2");
 
-Console.WriteLine($"{player.Name} chose {player.Choice}");
+var playerList = new List<Player> { player1, player2 };
+foreach (var player in playerList)
+{
+    player.Choose();
+}
+
+Console.WriteLine($"{player1.Name} chose {player1.Choice}");
+Console.WriteLine($"{player2.Name} chose {player2.Choice}");
